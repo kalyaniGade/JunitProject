48a49f5f07308f58844902eb9a985747b3e73535
C-PN15 <C-PN15@DAL-8CO097-9W7.wks.tsh.thomson.com>
2019-06-17 11:03:22 +0530

    from eclipse by kalyani Gade

---
+++ src/virtusa/truven/JunitMainRunner/JunitMainRunnerTest.java
@@ -0,0 +1,14 @@
+package virtusa.truven.JunitMainRunner;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JunitMainRunnerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
